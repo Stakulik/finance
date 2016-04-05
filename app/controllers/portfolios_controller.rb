c05aaa494c5d9d23f44b33dd57d462f7e2a21336
Stakulik <stakulik@mail.ru>
2016-04-05 22:35:35 +0300

    Add functionality to stocks

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,7 @@ class PortfoliosController < ApplicationController
   before_filter :authenticate_user!
   
   def index
+    @portfolios = Portfolio.eager_load(:stocks).where("user_id == ?", current_user.id)
   end
 
   def new
@@ -9,4 +10,20 @@ class PortfoliosController < ApplicationController
 
   def edit
   end
+
+  def show
+    @portfolio = Portfolio.eager_load(:stocks).find(params[:id])
+  end
+
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :name,
+      :description,
+      :user_id
+    )
+  end
+
 end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,66 @@
 class StocksController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
+  before_filter :check_for_cancel, :only => [:create, :update]
 
   def new
+    @portfolio = get_portfolio
+    @stock = @portfolio.stocks.build
+  end
+
+  def create
+    @portfolio = get_portfolio
+    @stock = @portfolio.stocks.build(stock_params)
+
+    if @stock.save
+      redirect_to @portfolio
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @stock = Stock.find(params[:id])
+    @portfolio = get_portfolio
   end
+
+  def update
+    @stock = Stock.find(params[:id])
+    @portfolio = get_portfolio
+
+    if @stock.update_attributes(stock_params)
+      redirect_to @portfolio
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Stock.find(params[:id]).destroy
+    redirect_to :back
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(
+      :name,
+      :amount,
+      :portfolio_id
+    )
+  end
+
+  def get_portfolio
+    if params[:portfolio_id]
+      Portfolio.find(params[:portfolio_id])
+    else
+      params[:stock][:portfolio_id]
+    end
+  end
+
+  def check_for_cancel
+    if params[:commit] == "Отменить"
+      redirect_to portfolio_path(params[:stock][:portfolio_id])
+    end
+  end
+
 end
