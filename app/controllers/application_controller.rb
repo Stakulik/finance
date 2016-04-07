151218aaa567e19cd54bb57d4a3d9f4818008906
Stakulik <stakulik@mail.ru>
2016-04-08 00:25:02 +0300

    Update errors explanations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
 
   def after_sign_in_path_for(resource)
@@ -11,4 +10,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+private
+
+  def render_404(exception)
+    respond_to do |format|
+      format.html { render template: 'errors/error_404', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Finance
   class Application < Rails::Application
+    I18n.enforce_available_locales = false
+    config.i18n.locale = :ru
+    config.i18n.default_locale = :ru
+    config.i18n.enforce_available_locales = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
